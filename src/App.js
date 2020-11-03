36013135e05a0c45c23d7cfe9fbf61be17beb914
Pinki LNU <PinkiLNU@corp.fairisaac.com>
2020-11-03 23:47:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. It is just a sample web app for trial purpose.
         </p>
         <a
           className="App-link"
